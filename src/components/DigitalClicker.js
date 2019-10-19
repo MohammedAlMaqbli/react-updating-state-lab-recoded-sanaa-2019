e3c8c34fa088fd832f1743ac9818fe42fd5666ed
MohammedAlMaqbli <Mohammed.AlMaqbli@gmail.com>
2019-10-19 08:53:23 -0700

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,29 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react'
+export default class DigitalClicker extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            timesClicked: 0
+        }
+
+    }
+         
+    handelClick = () => {
+        let num = this.state.timesClicked + 1 ;
+        this.setState({
+            timesClicked: num
+        })
+    }
+    render() {
+        return (
+       
+        <div>
+            <button onClick={this.handelClick}>
+                {this.state.timesClicked}
+            </button>
+        </div>
+    )
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,59 @@
-// Code YouTubeDebugger Component Here
+import React, { Component } from 'react'
+
+export class YouTubeDebugger extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: '1080p'
+                }
+            }
+
+        }
+    }
+    handleBitrate = () => {
+        this.setState(
+            {
+                errors: [],
+                user: null,
+                settings: {
+                    bitrate: 12,
+                    video: {
+                        resolution: '1080p'
+                    }
+                }
+
+            }
+        )
+    }
+    handleResolution = () => {
+        let bitr = this.state.settings.bitrate
+        this.setState({
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: bitr,
+                video: {
+                    resolution: '720p'
+                }
+            }
+
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <button className="bitrate" onClick={this.handleBitrate}>Change bitrate</button>
+                <button className="resolution" onClick={this.handleResolution}></button>
+            </div>
+        )
+    }
+}
+
+export default YouTubeDebugger 
